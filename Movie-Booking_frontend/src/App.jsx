1506cdeb57e1627c5d87a91326eb28cd6d7363b5
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-07-17 10:01:04 +0530

    seat layout

--- Movie-Booking_frontend/src/App.jsx
+++ Movie-Booking_frontend/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
   return (
    <>
     {!isAdminRoute && <Navbar/>}
+    <Toaster position="top-center" />
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/movies' element={<Movies/>}/>
--- Movie-Booking_frontend/src/pages/MovieDetails.jsx
+++ Movie-Booking_frontend/src/pages/MovieDetails.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import timeFormat from '../lib/timeFormat'
 import BlurCircle from '../components/BlurCircle'
 import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react'
+import DateSelect from '../components/DateSelect'
+import MovieCard from '../components/MovieCard'
+import Loading from '../components/Loading'
 
 function MovieDetails() {
+  const navigate=useNavigate()
   const {id}=useParams()
   const [show,setShow]=useState(null)
   const getShow=async()=>{
     const show=dummyShowsData.find(show=>show.id==id)
-    setShow({
+    if(show){
+        setShow({
       movie:show,
       dateTime:dummyDateTimeData
     })
+  }
   }
   useEffect(()=>{
     getShow()
@@ -43,16 +49,39 @@ function MovieDetails() {
     <PlayCircleIcon className="w-5 h-5"/>
     Watch Trailer
     </button>
-  <a href="" className='px-10 py-3 ext-sm bg-primary hover:bg-primary-dull transition rounded-md front-mediumcursor-pointer active:scale-95'>Buy Tickets</a>
+  <a href="#dateSelect" className='px-10 py-3 ext-sm bg-primary hover:bg-primary-dull transition rounded-md front-mediumcursor-pointer active:scale-95'>Buy Tickets</a>
   <button className='bg-gray-700 p-2.5 rounded-full transtion cursor-pointer active:scale-95'>
     <Heart className={`w-5 h-5`}/>
   </button>
     </div>
   </div >
 </div>
+ <p className='text-lg font-medium mt-20'>Your Favourite Cast</p>
+ <div className='overflow-x-auto no-scrollbar mt-8 pb-4'>
+<div className='flex items-center gap-4 w-max px-4'>
+{show.movie.casts.slice(0,12).map((cast,index)=>(
+  <div key={index} className='flex flex-col items-center text-center'>
+    <img src={cast.profile_path} alt='' className='rounded-full h-20 md:h-20 aspect-square object-cover'/>
+    <p className='font-medium text-xs mt-3'>{cast.name}</p>
+  </div>
+))}
+</div>
+ </div>
+<DateSelect dateTime={show.dateTime} id={id}/>
+<p className='text-lg font-medium mt-20 mb-8'>You May Also Like</p>
+<div className='flex flex-wrap max-sm:justify-center gap-8'>
+  {dummyShowsData.slice(0,4).map((movie, index) => (
+    <MovieCard key={index} movie={movie} />
+  ))}
+</div>
+<div className='flex justify-center mt-20'>
+  <button onClick={() => {navigate('/movies');scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>
+    Show more
+  </button>
+</div>
 
     </div>
-  ):<div>Loading...</div>
+  ):<div><Loading/></div>
 
   
 }
