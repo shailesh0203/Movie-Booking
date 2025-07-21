c7d0be30c8228e541e496a5f8aab007709f8d38f
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2025-07-21 11:12:29 +0530

    frontend

---
+++ Movie-Booking_frontend/src/components/admin/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Title({text1,text2}) {
+  return (
+   <h1 className='font-medium text-2xl'>
+    {text1} <span className='undelerline text-primary'>
+        {text2}
+    </span>
+   </h1>
+  )
+}
+
+export default Title
