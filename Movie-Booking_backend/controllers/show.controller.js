98b95bc0b48f4fc618ff672d698a63eea1c03e13
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2025-07-26 18:50:06 +0530

    bug resolve

--- Movie-Booking_backend/controllers/show.controller.js
+++ Movie-Booking_backend/controllers/show.controller.js
@@ -1,9 +1,21 @@
+import axios from "axios";
 
+export const getNowPlayingMovies = async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://api.themoviedb.org/3/movie/now_playing',
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+        }
+      }
+    );
 
-export const getNowPlayingMovies=async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+    const movies = response.data.results;  // FIXED
+    res.json({ success: true, movies });
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
--- Movie-Booking_backend/server.js
+++ Movie-Booking_backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 
 const app = express();
 const PORT =  process.env.PORT || 3000
@@ -17,10 +18,12 @@ app.use(express.json());
 app.use(clerkMiddleware())
 
 
+
 //API Routess
 app.get('/', (req, res) => {
   res.send('Welcome to the Movie Booking API');
 })
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/show',showRouter)
 
 app.listen(PORT,()=>console.log(`Server listening at http://localhost:${PORT}`));
